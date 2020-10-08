a57ee4c55cc2fc41fbe2a92cb95eebf43e172c0f
Abhay Sudan <abhaysudan007@gmail.com>
2020-10-09 01:17:15 +0530

    Sign in / Sign out/ firebase authentication / firebase deploy / firebase hosting

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,8 +1,40 @@
 import React from "react";
 import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
 
-function CheckoutProduct() {
-  return <div className="checkoutProduct"></div>;
+function CheckoutProduct({ id, image, title, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    //remove item from basket
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
+
+  return (
+    <div className="checkoutProduct">
+      <img className="checkoutProduct__image" src={image} />
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+          <small>$</small>
+          <small>{price}</small>
+        </p>
+        <p className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
+        </p>
+        <button className="checkoutProduct__button" onClick={removeFromBasket}>
+          Remove from basket
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default CheckoutProduct;
--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,18 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyChNpgxHO1MzBtqwFG7EUk5A18cqRg5ePU",
-    authDomain: "clone-12aa0.firebaseapp.com",
-    databaseURL: "https://clone-12aa0.firebaseio.com",
-    projectId: "clone-12aa0",
-    storageBucket: "clone-12aa0.appspot.com",
-    messagingSenderId: "907440856821",
-    appId: "1:907440856821:web:b099cfa5ac19aedb81efbc"
-  };
+  apiKey: "AIzaSyChNpgxHO1MzBtqwFG7EUk5A18cqRg5ePU",
+  authDomain: "clone-12aa0.firebaseapp.com",
+  databaseURL: "https://clone-12aa0.firebaseio.com",
+  projectId: "clone-12aa0",
+  storageBucket: "clone-12aa0.appspot.com",
+  messagingSenderId: "907440856821",
+  appId: "1:907440856821:web:b099cfa5ac19aedb81efbc",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
